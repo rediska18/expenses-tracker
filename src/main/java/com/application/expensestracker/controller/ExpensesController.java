85b970d2ca5d45f469860885c3179f6a6af3c981
Yaroslav Trubin <yaroslav.trubin@gmail.com>
2021-03-01 00:15:19 +0200

    Додано/оновлено тестові запити

--- src/main/java/com/application/expensestracker/controller/ExpensesController.java
+++ src/main/java/com/application/expensestracker/controller/ExpensesController.java
@@ -7,7 +7,13 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.nio.charset.Charset;
 import java.util.List;
@@ -23,32 +29,30 @@ public class ExpensesController {
     @GetMapping(path = "/expenses", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<Expenses>> get() {
         List<Expenses> expenses = expensesService.findAll();
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.setContentType(new MediaType(MediaType.APPLICATION_JSON, Charset.forName("UTF-8")));
-        return new ResponseEntity<List<Expenses>>(expenses, httpHeaders, HttpStatus.OK);
-    }
-
-    @PostMapping(path = "/expenses", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Expenses> save(@RequestBody Expenses expenses){
-        Expenses storedExpenses = expensesService.save(expenses);
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.setContentType(new MediaType(MediaType.APPLICATION_JSON, Charset.forName("UTF-8")));
-        return new ResponseEntity<Expenses>(httpHeaders, HttpStatus.OK);
+        return new ResponseEntity<List<Expenses>>(expenses, prepareHttpResponseHeaders(MediaType.APPLICATION_JSON), HttpStatus.OK);
     }
 
     @GetMapping(path = "/expenses/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Expenses> get(@PathVariable("id") Long id){
         Expenses expenses = expensesService.findById(id);
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.setContentType(new MediaType(MediaType.APPLICATION_JSON, Charset.forName("UTF-8")));
-        return new ResponseEntity<Expenses>(expenses, httpHeaders, HttpStatus.OK);
+        return new ResponseEntity<Expenses>(expenses, prepareHttpResponseHeaders(MediaType.APPLICATION_JSON), HttpStatus.OK);
+    }
+
+    @PostMapping(path = "/expenses", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Expenses> save(@RequestBody Expenses expenses){
+        Expenses storedExpenses = expensesService.save(expenses);
+        return new ResponseEntity<Expenses>(prepareHttpResponseHeaders(MediaType.APPLICATION_JSON), HttpStatus.OK);
     }
 
     @DeleteMapping(path = "/expenses/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> delete(@PathVariable("id") Long id){
         expensesService.delete(id);
+        return new ResponseEntity<String>("Expense is deleted successfully",prepareHttpResponseHeaders(MediaType.TEXT_PLAIN), HttpStatus.OK);
+    }
+
+    private HttpHeaders prepareHttpResponseHeaders(MediaType mediaType){
         HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.setContentType(new MediaType(MediaType.TEXT_PLAIN, Charset.forName("UTF-8")));
-        return new ResponseEntity<String>("Expense is deleted successfully", HttpStatus.OK);
+        httpHeaders.setContentType(new MediaType(mediaType, Charset.forName("UTF-8")));
+        return httpHeaders;
     }
 }
--- src/test/java/com/application/expensestracker/controller/ExpensesControllerTest.java
+++ src/test/java/com/application/expensestracker/controller/ExpensesControllerTest.java
@@ -5,7 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
+import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.event.annotation.BeforeTestMethod;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -22,15 +24,18 @@ class ExpensesControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @BeforeTestMethod
+    void setUp(){
+    }
+
     @Test
     @Sql(scripts = "classpath:db/h2/test-data.dml")
-    void get() throws Exception {
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
-
+    void testGetMethod() throws Exception {
+        HttpHeaders requestHttpHeaders = new HttpHeaders();
+        requestHttpHeaders.setContentType(MediaType.APPLICATION_JSON);
         mockMvc.perform(MockMvcRequestBuilders
                 .get(ExpensesController.ROOT_URL + "/expenses")
-                .headers(httpHeaders))
+                .headers(requestHttpHeaders))
                 .andExpect(status().isOk())
                 .andDo(mvcResult -> {
                     MockHttpServletResponse mockHttpServletResponse = mvcResult.getResponse();
@@ -41,14 +46,21 @@ class ExpensesControllerTest {
     }
 
     @Test
-    void save() {
+    void testGetMethodWithParams() {
     }
 
     @Test
-    void testGet() {
+    void testSaveMethod() throws Exception {
+
     }
 
     @Test
-    void delete() {
+    void testDeleteMethod() {
     }
+
+    private MockHttpServletRequest prepareHttpServletRequest(){
+        MockHttpServletRequest mockHttpServletRequest = new MockHttpServletRequest();
+        mockHttpServletRequest.setContent("{\"id\":1,\"expenses\":\"Not healthy food\",\"description\":\"alcohol\",\"amount\":300.00}".getBytes(Charset.forName("UTF-8")));
+        return mockHttpServletRequest;
+    };
 }
